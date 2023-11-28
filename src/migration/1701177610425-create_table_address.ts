901832efe968a0b48cfba601f077584c6d34eb42
Robson Ribeiro <robson.riber777@gmail.com>
2023-11-28 11:00:58 -0300

    Criado migration address

--- src/migration/1701177610425-create_table_address.ts
+++ src/migration/1701177610425-create_table_address.ts
@@ -3,9 +3,24 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class CreateTableAddress1701177610425 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+        CREATE TABLE address (
+            id integer NOT NULL AUTO_INCREMENT,
+            user_id integer NOT NULL,
+            complement VARCHAR(50),
+            number integer NOT NULL,
+            cep VARCHAR(10) NOT NULL,
+            city_id integer NOT NULL,
+            created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            updated_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            primary key (id),
+            foreign key (user_id) references user(id),
+            foreign key (city_id) references city(id)
+        );`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query('drop table address');
     }
 
 }
